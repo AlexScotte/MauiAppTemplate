dd130297636e8483b0b8a7d3e0b5a17c1dfc296e
ascotte <ascotte@also.lan>
2022-03-29 15:35:23 +0200

    Removing unused project

--- MauiAppTemplate.Common/Platforms/Android/PlatformClass1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MauiAppTemplate.Common
-{
-    // All the code in this file is only included on Android.
-    public class PlatformClass1
-    {
-    }
-}
--- MauiAppTemplate.Common/Platforms/Windows/PlatformClass1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MauiAppTemplate.Common
-{
-    // All the code in this file is only included on Windows.
-    public class PlatformClass1
-    {
-    }
-}
