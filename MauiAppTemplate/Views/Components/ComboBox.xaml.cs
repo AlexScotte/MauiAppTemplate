49466525178deb9d4a91d91c4ab1a3f115a9589a
ascotte <ascotte@also.lan>
2022-03-30 15:35:09 +0200

    Managing color of combobox control

--- MauiAppTemplate/Views/Components/ComboBox.xaml.cs
+++ MauiAppTemplate/Views/Components/ComboBox.xaml.cs
@@ -2,12 +2,68 @@ namespace MauiAppTemplate.Views.Components;
 
 public partial class ComboBox : ContentView
 {
-    public static readonly BindableProperty ItemsProperty =
+    #region Bindable properties
+
+    public new static readonly BindableProperty BackgroundColorProperty =
     BindableProperty.Create(
-        nameof(Items),
-        typeof(List<ComboBox.Item>),
+        nameof(BackgroundColor),
+        typeof(Color),
         typeof(ComboBox),
-        defaultValue: null);
+        defaultValue: Colors.White);
+
+    public new Color BackgroundColor
+    {
+        get { return (Color)GetValue(BackgroundColorProperty); }
+        set { SetValue(BackgroundColorProperty, value); }
+    }
+
+    public static readonly BindableProperty SelectorBackgroundColorProperty =
+BindableProperty.Create(
+    nameof(SelectorBackgroundColor),
+    typeof(Color),
+    typeof(ComboBox),
+    defaultValue: Colors.White);
+
+    public Color SelectorBackgroundColor
+    {
+        get { return (Color)GetValue(SelectorBackgroundColorProperty); }
+        set { SetValue(SelectorBackgroundColorProperty, value); }
+    }
+
+    public static readonly BindableProperty TextColorProperty =
+        BindableProperty.Create(
+            nameof(TextColor),
+            typeof(Color),
+            typeof(ComboBox),
+            defaultValue: Colors.Black);
+
+    public Color TextColor
+    {
+        get { return (Color)GetValue(TextColorProperty); }
+        set { SetValue(TextColorProperty, value); }
+    }
+
+    public static readonly BindableProperty SelectorTextColorProperty =
+        BindableProperty.Create(
+            nameof(SelectorTextColor),
+            typeof(Color),
+            typeof(ComboBox),
+            defaultValue: Colors.Black);
+
+    public Color SelectorTextColor
+    {
+        get { return (Color)GetValue(SelectorTextColorProperty); }
+        set { SetValue(SelectorTextColorProperty, value); }
+    }
+
+    public static readonly BindableProperty ItemsProperty =
+        BindableProperty.Create(
+            nameof(Items),
+            typeof(List<ComboBox.Item>),
+            typeof(ComboBox),
+            defaultValue: null);
+
+
 
     public List<ComboBox.Item> Items
     {
@@ -15,16 +71,27 @@ public partial class ComboBox : ContentView
         set { SetValue(ItemsProperty, value); }
     }
 
+    public static readonly BindableProperty SelectedItemProperty =
+        BindableProperty.Create(
+            nameof(SelectedItem),
+            typeof(ComboBox.Item),
+            typeof(ComboBox),
+            defaultValue: null);
+
+    public ComboBox.Item SelectedItem
+    {
+        get { return (ComboBox.Item)GetValue(SelectedItemProperty); }
+        set { SetValue(SelectedItemProperty, value); }
+    }
+    #endregion
 
     public bool IsExpanded { get; set; }
-    public string SelectedItem { get; set; }
-
 
     public ComboBox()
-	{
-		InitializeComponent();
+    {
+        InitializeComponent();
         Items = new List<Item>();
-	}
+    }
 
     private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
     {
@@ -39,7 +106,7 @@ public partial class ComboBox : ContentView
     private void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
     {
         IsExpanded = false;
-        SelectedItem = e.SelectedItem.ToString();
+        SelectedItem = e.SelectedItem as ComboBox.Item;
 
         OnPropertyChanged(nameof(IsExpanded));
         OnPropertyChanged(nameof(SelectedItem));
@@ -49,6 +116,6 @@ public partial class ComboBox : ContentView
     {
         public string Image { get; set; }
         public string Label { get; set; }
-        public bool IsEnabled { get; set; }
+        public bool IsEnabled { get; set; } = true;
     }
 }
